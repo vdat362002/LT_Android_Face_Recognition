f096ae429cc842866013935b6026e9ff1d09cef5
vdat362002 <vandatdinh2@gmail.com>
2023-05-31 21:37:28 +0700

    update

--- app/src/main/java/com/atharvakale/facerecognition/splash_screen.java
+++ app/src/main/java/com/atharvakale/facerecognition/splash_screen.java
@@ -12,14 +12,14 @@ public class splash_screen extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash_screen);
-        Handler handler=new Handler();
+        Handler handler = new Handler();
         handler.postDelayed(new Runnable() {
             @Override
             public void run() {
-                Intent intent =new Intent(splash_screen.this,uploadimage.class);
+                Intent intent = new Intent(splash_screen.this, MainActivity.class);
                 finish();
                 startActivity(intent);
             }
-        },2500);
+        }, 2500);
     }
 }
